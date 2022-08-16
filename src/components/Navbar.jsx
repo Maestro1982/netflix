f0017d66f1d7cb2effd3d3e9d698b2dde42ec287
Maestro1982 <kris.tote@telenet.be>
2022-08-16 16:33:45 +0200

    create main, row, home page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,11 @@
 import React from 'react';
-import logo from '../images/netflix.png';
 
 function Navbar() {
   return (
     <div className='flex items-center justify-between p-4 z-[100] w-full absolute'>
-      <img
-        className='cursor-pointer'
-        src={logo}
-        alt='Netflix Logo'
-        width={170}
-        height={170}
-      />
+      <h1 className='text-[#e50914] text-4xl font-bold cursor-pointer ml-4'>
+        NETFLIX
+      </h1>
       <div>
         <button className='pr-4 text-white'>Sign In</button>
         <button className='bg-[#e50914] px-4 py-2 text-white rounded-sm'>
---
+++ src/components/Row.jsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useRef, useState } from 'react';
+import { FaHeart, FaRegHeart } from 'react-icons/fa';
+import axios from 'axios';
+
+function Row({ title, fetchURL }) {
+  const [movies, setMovies] = useState([]);
+  const [isLiked, setIsLiked] = useState(false);
+  const slider = useRef();
+
+  useEffect(() => {
+    axios.get(fetchURL).then((response) => {
+      setMovies(response.data.results);
+    });
+  }, [fetchURL]);
+
+  return (
+    <>
+      <h2 className='text-white font-bold md:text-xl p-4'>{title}</h2>
+      <div className='relative flex items-center' ref={slider}>
+        <div>
+          {movies?.map((movie, id) => (
+            <div
+              key={id}
+              className='w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2'
+            >
+              <img
+                className='w-full h-auto block'
+                src={`https://image.tmdb.org/t/p/w500/${movie?.backdrop_path}`}
+                alt={movie?.title}
+              />
+              <div className='absolute top-0 left-0 w-full h-full hover:bg-black/50 opacity-0 hover:opacity-100 text-white'>
+                <p className='white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center'>
+                  {movie?.title}
+                </p>
+                <p>
+                  {isLiked ? (
+                    <FaHeart className='absolute top-4 left-4 text-gray-300' />
+                  ) : (
+                    <FaRegHeart className='absolute top-4 left-4 text-gray-300' />
+                  )}
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Row;
